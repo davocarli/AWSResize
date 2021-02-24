c412c5ff5bd0f12738b43291771a3d21e5da7b08
David Carli-Arnold <davocarli@gmail.com>
2021-02-23 22:46:46 -0800

    Added Archive

--- CreateThumbnail/CreateThumbnail.py
+++ CreateThumbnail/CreateThumbnail.py
@@ -15,13 +15,13 @@ def resize_image(image_path, resized_path, size):
 def handler(event, context):
     for record in event['Records']:
         bucket = record['s3']['bucket']['name']
-        key = record['s3']['object']['key'] 
-        download_path = '/tmp/{}{}'.format(uuid.uuid4(), key)
-        upload_path = '/tmp/resized-{}'.format(key)
+        key = record['s3']['object']['key']
+        right_key = key.rsplit('/', 1)[1]
+        download_path = '/tmp/{}{}'.format(uuid.uuid4(), right_key)
+        upload_path = '/tmp/resized-{}'.format(right_key)
         
         s3_client.download_file(bucket, key, download_path)
-        resize_image(download_path, upload_path, 1000)
-        s3_client.upload_file(upload_path, bucket, key.replace('fullSize/', '1000'))
 
-        resize_image(download_path, upload_path, 300)
-        s3_client.upload_file(upload_path, bucket, key.replace('fullSize/', '300'))
+        for size in [2500, 1500, 1000, 500, 300, 128]:
+            resize_image(download_path, upload_path, size)
+            s3_client.upload_file(upload_path, bucket, key.replace('fullSize/', str(size) + '/'))
